8bb12ec0560301f1b28ca34ee85599b450072625
Mao Kim Huong <maokimhuong@MAos-Laptop.local>
2024-06-27 22:12:29 +0700

    removeanaly

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
-import { Analytics } from "@vercel/analytics/react";
+
 import Layout from "../components/Layout";
 import Transition from "../components/Transition";
 
@@ -10,8 +10,7 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <>
-      <Analytics>
+    
         <Layout>
           <AnimatePresence mode="wait">
             <motion.div key={router.route} className="h-full">
@@ -20,9 +19,8 @@ function MyApp({ Component, pageProps }) {
             </motion.div>
           </AnimatePresence>
         </Layout>
-      </Analytics>
-    </>
-  );
+     
+  )
 }
 
 export default MyApp;
