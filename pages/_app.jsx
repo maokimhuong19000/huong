980ab5a628f519702859dbae22d2a43e8306ce7f
Mao Kim Huong <maokimhuong@MAos-Laptop.local>
2024-06-27 20:31:28 +0700

    analy

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
-
+import { Analytics } from "@vercel/analytics/react";
 import Layout from "../components/Layout";
 import Transition from "../components/Transition";
 
@@ -10,14 +10,18 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <Layout>
-      <AnimatePresence mode="wait">
-        <motion.div key={router.route} className="h-full">
-          <Transition />
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </Layout>
+    <>
+      <Analytics>
+        <Layout>
+          <AnimatePresence mode="wait">
+            <motion.div key={router.route} className="h-full">
+              <Transition />
+              <Component {...pageProps} />
+            </motion.div>
+          </AnimatePresence>
+        </Layout>
+      </Analytics>
+    </>
   );
 }
 
