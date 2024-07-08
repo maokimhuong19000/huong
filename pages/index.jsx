75f51501fcb6b2f997346c856304014ee80de3ab
Mao Kim Huong <131876915+maokimhuong19000@users.noreply.github.com>
2024-07-08 09:27:33 +0700

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -32,10 +32,7 @@ const Home = () => {
             exit="hidden"
             className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
           >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
-            exercitationem harum, quia nulla temporibus deleniti libero veniam
-            vero beatae numquam ducimus illum ab similique ipsam tempore fugit
-            quod laudantium debitis.
+            Somthing About Me
           </motion.p>
 
           {/* btn */}
